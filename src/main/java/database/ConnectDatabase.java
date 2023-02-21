827f35b46737006816a8a170bdd3292c5ddcd98e
joeymach <joey.mach@shopify.com>
2023-02-21 18:10:07 -0500

    connect db p2

--- src/main/java/database/ConnectDatabase.java
+++ src/main/java/database/ConnectDatabase.java
@@ -2,82 +2,76 @@ package database;
 
 import java.io.*;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Date;
 import static java.lang.Integer.parseInt;
 
 public class ConnectDatabase {
-    private Connection connect = null;
+    private Connection connection = null;
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
 
     public void readDataBase() throws Exception {
+        int batchSize=20;
+        String filePath="nhpi.csv";
+
         try {
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Setup the connection with the DB
-            connect = DriverManager
+            connection = DriverManager
                     .getConnection("jdbc:mysql://localhost/nhpi?"
                             + "user=root&password=");
 
+            connection.setAutoCommit(false);
 
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            close();
-        }
+            String sql="insert into nhpi(year,month,city,province,value) values(?,?,?,?,?)";
 
-    }
+            PreparedStatement statement=connection.prepareStatement(sql);
 
-    private void writeMetaData(ResultSet resultSet) throws SQLException {
-        //  Now get some metadata from the database
-        // Result set get the result of the SQL query
+            BufferedReader lineReader=new BufferedReader(new FileReader(filePath));
 
-        System.out.println("The columns in the table are: ");
+            String lineText=null;
+            int count=0;
 
-        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
-        }
-    }
+            int length = 0;
 
-    private void writeResultSet(ResultSet resultSet) throws SQLException {
-        // ResultSet is initially before the first data set
-        while (resultSet.next()) {
-            // It is possible to get the columns via name
-            // also possible to get the columns via the column number
-            // which starts at 1
-            // e.g. resultSet.getSTring(2);
-            String user = resultSet.getString("myuser");
-            String website = resultSet.getString("webpage");
-            String summary = resultSet.getString("summary");
-            Date date = resultSet.getDate("datum");
-            String comment = resultSet.getString("comments");
-            System.out.println("User: " + user);
-            System.out.println("Website: " + website);
-            System.out.println("summary: " + summary);
-            System.out.println("Date: " + date);
-            System.out.println("Comment: " + comment);
-        }
-    }
+            lineReader.readLine();
+            while ((lineText=lineReader.readLine())!=null){
+                String[] data=lineText.split(",");
 
-    // You need to close the resultSet
-    private void close() {
-        try {
-            if (resultSet != null) {
-                resultSet.close();
+                int year = parseInt(data[0].split("-")[0]);
+                int month = parseInt(data[0].split("-")[1]);
+
+                String[] location = data[1].replaceAll("[\"]", "").split(", ");
+
+                if(location.length > 1) {
+                    length = location.length - 1;
+                } else {
+                    length = 0;
+                }
+
+                String city = String.join(", ", Arrays.copyOfRange(location, 0, location.length - 1));
+                String province = location[location.length - 1];
+
+                System.out.println("city " + city);
+                System.out.println("prov  " + province);
+
+                if(count%batchSize==0){
+                    statement.executeBatch();
+                }
             }
-
-            if (statement != null) {
-                statement.close();
-            }
-
-            if (connect != null) {
-                connect.close();
-            }
-        } catch (Exception e) {
-
+            lineReader.close();
+            statement.executeBatch();
+            connection.commit();
+            connection.close();
+            System.out.println("Data has been inserted successfully.");
         }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
+
     }
 
 }
