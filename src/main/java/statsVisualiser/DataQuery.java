92d168857983afd5b6387f753a4e2e97fed7b4e1
joeymach <joey.mach@shopify.com>
2023-03-25 19:14:48 -0400

    [Data] extracting query functionality to new class

---
+++ src/main/java/statsVisualiser/DataQuery.java
@@ -0,0 +1,48 @@
+package statsVisualiser;
+
+import database.ConnectDatabase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DataQuery {
+    public static String[][] getDataFromDatabase(String province, String city, String fromYear,
+                                          String fromMonth, String toYear, String toMonth) throws SQLException {
+        String[][] dataCleaned;
+        if(!(fromYear == "All" && toYear == "All" && fromMonth == "All" && toMonth == "All") &&
+                (Integer.parseInt(fromYear) > Integer.parseInt(toYear) ||
+                        (Integer.parseInt(fromYear) == Integer.parseInt(toYear) &&
+                                (Integer.parseInt(fromMonth) > (Integer.parseInt(toMonth)))))) {
+            System.out.println("Dates passed is invalid");
+            dataCleaned = new String[][]{};
+        } else {
+            //ConnectDatabase mysql = new ConnectDatabase();
+            Connection connection = ConnectDatabase.getConnection();
+
+            String query = ConnectDatabase.getQuery(province, city, fromYear, fromMonth, toYear, toMonth);
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+
+            ArrayList<String[]> data = new ArrayList<String[]>();
+            while (result.next())
+            {
+                data.add(new String[]{result.getString(1),
+                        result.getString(2),
+                        result.getString(3),
+                        result.getString(4),
+                        result.getString(5)});
+            }
+            connection.close();
+
+            dataCleaned = new String[data.size()][5];
+            Arrays.setAll(dataCleaned, data::get);
+        }
+
+        return dataCleaned;
+    }
+}
